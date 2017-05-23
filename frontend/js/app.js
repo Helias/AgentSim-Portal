cccaac475d341a41eb5c880d0fb6c5bd9bd6332b
Helias <stefanoborzi32@gmail.com>
2017-05-23 19:20:11 +0200

    Added Notifications library, implemented login

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,29 @@
-var app = angular.module('app', [ 'ui.router', 'ui.bootstrap' ]);
+var app = angular.module('app', [ 'ui.router', 'ui.bootstrap', 'ui-notification' ]);
 
 app.controller('homeController', function ($scope) {
   $scope.helloworld = "Hello world by AngularJS";
+});
+
+app.controller('loginController', function ($scope, $state, $http, Notification) {
+  var path = "http://localhost:8080/";
+
+  $scope.loginUser = function() {
+    $http({
+      url: path + "api/authenticate",
+      method: "POST",
+      data: { 'name' : $scope.username, 'password' : $scope.password }
+    })
+    .then(function(response) {
+      $scope.result = response.data;
+      if ($scope.result["result"]){
+        $localStorage.token = $scope.result["result"];
+        $state.go("/");
+      }
+      else if ($scope.result["error"])
+        Notification.error($scope.result["error"]);
+    },
+    function(response) {
+      console.log("http Request error!");
+    });
+  };
 });
--- frontend/js/routes.js
+++ frontend/js/routes.js
@@ -10,11 +10,14 @@ app.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: 'partials/home.html',
       controller: 'homeController'
     })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'partials/login.html',
+      controller: 'loginController'
+   })
    .state('page2', {
       url: '/page2',
       templateUrl: 'partials/page2.html',
       controller: 'homeController'
-    })
-
-    ;
+    });
 });
