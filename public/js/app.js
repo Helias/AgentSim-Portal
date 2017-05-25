686f54500af5ae87314f9cde0b3cd89705d95ad5
Helias <stefanoborzi32@gmail.com>
2017-05-26 00:02:50 +0200

    Completed login

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,23 @@ var app = angular.module('app', [ 'ui.router',
                                   'angular-loading-bar'
                                 ]);
 
+app.run(function($rootScope, $localStorage){
+  $rootScope.user = ($localStorage.user != null && $localStorage.user.token != "") ? $localStorage.user : "";
+});
+
+
 app.controller('homeController', function ($scope) {
   $scope.helloworld = "Hello world by AngularJS";
 });
 
-app.controller('loginController', function ($scope, $state, $http, $localStorage, Notification) {
+app.controller('loginController', function ($scope, $rootScope, $state, $http, $localStorage, Notification) {
 
   var path = "http://localhost:8080/api";
+
+  if ($rootScope.user != null && $rootScope.user.token != "") {
+    $rootScope.user = null;
+    $localStorage.user = null;
+  }
 
   $scope.loginUser = function() {
 
@@ -22,9 +32,14 @@ app.controller('loginController', function ($scope, $state, $http, $localStorage
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
     }).then(
         function(res) {
-          $localStorage.token = res.data.token;
-          $state.go("home");
-          Notification.success("Logged successfully");
+          if (res.data.success) {
+            $localStorage.user = { token : res.data.token, name : $scope.username };
+            $rootScope.user = $localStorage.user;
+            $state.go("home");
+            Notification.success("Logged successfully");
+          }
+          else
+            Notification.error("Wrong username or password!");
         },
         function(err) {
           Notification.error("Error!");
